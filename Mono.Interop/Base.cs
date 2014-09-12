46d29c8c4a85465307be015345104cac12bf6718
John Bradley <jrb@turrettech.com>
2014-09-12 18:09:44 -0500

    I'm bored of this

--- Mono.Interop/Base.cs
+++ Mono.Interop/Base.cs
@@ -1,50 +1,76 @@
-﻿using System;
+﻿#define LOG_METHOD_NAMES 
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace Mono.Interop.Internal
+namespace Mono.Interop
 {
-	internal class Base
+	public enum LogLevel
 	{
-		public enum LogLevel
-		{
-			/**
-			 * Use if there's a problem that can potentially affect the program,
-			 * but isn't enough to require termination of the program.
-			 *
-			 * Use in creation functions and core subsystem functions.  Places that
-			 * should definitely not fail.
-			 */
-			LOG_ERROR = 100,
+		/**
+		 * Use if there's a problem that can potentially affect the program,
+		 * but isn't enough to require termination of the program.
+		 *
+		 * Use in creation functions and core subsystem functions.  Places that
+		 * should definitely not fail.
+		 */
+		LOG_ERROR = 100,
 
-			/**
-			 * Use if a problem occurs that doesn't affect the program and is
-			 * recoverable.
-			 *
-			 * Use in places where where failure isn't entirely unexpected, and can
-			 * be handled safely.
-			 */
-			LOG_WARNING = 200,
+		/**
+		 * Use if a problem occurs that doesn't affect the program and is
+		 * recoverable.
+		 *
+		 * Use in places where where failure isn't entirely unexpected, and can
+		 * be handled safely.
+		 */
+		LOG_WARNING = 200,
 
-			/**
-			 * Informative essage to be displayed in the log.
-			 */
-			LOG_INFO = 300,
+		/**
+		 * Informative essage to be displayed in the log.
+		 */
+		LOG_INFO = 300,
 
-			/**
-			 * Debug message to be used mostly by developers.
-			 */
-			LOG_DEBUG = 400
-		}
+		/**
+		 * Debug message to be used mostly by developers.
+		 */
+		LOG_DEBUG = 400
+	}
 
-		[DllImport ("obs", CallingConvention = CallingConvention.Cdecl)]
-		private static extern int blog (LogLevel logLevel, string message);
+	sealed partial class Base
+	{
+		[DllImport("obs")]
+		private static extern int blog(LogLevel logLevel, string message);
+	}
 
-		public static void Log (LogLevel logLevel, string format, 
+	sealed partial class Base
+	{
+
+		public static void Log(LogLevel logLevel, string format, 
 			params object[] args)
 		{
-			blog (logLevel, String.Format (format, args, null));
+			blog(logLevel, String.Format(format, args));
 		}
 
+		public static void LogError(string format, params object[] args)
+		{
+			Log(LogLevel.LOG_ERROR, format, args);
+		}
+
+		public static void LogWarning(string format, params object[] args)
+		{
+			Log(LogLevel.LOG_WARNING, format, args);
+		}
+
+		public static void LogDebug(string format, params object[] args)
+		{
+			Log(LogLevel.LOG_DEBUG, format, args);
+		}
+
+		public static void LogInfo(string format, params object[] args)
+		{
+			Log(LogLevel.LOG_INFO, format, args);
+		}
 	}
 }
-
--- obs-mono.c
+++ obs-mono.c
@@ -1,47 +1,59 @@
 #include <obs-module.h>
+#include <util/platform.h>
 
 #include "mono/jit/jit.h"
 #include "mono/metadata/assembly.h"
 #include "mono/metadata/mono-debug.h"
 #include "mono/metadata/mono-config.h"
+#include "mono/metadata/threads.h"
 
+#include <mach-o/dyld.h>
+#include <mach-o/getsect.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-mono", "en-US")
 
+struct obs_mono {
+	MonoDomain *domain;
+} mono;
+
 bool obs_module_load(void)
 {
-	mono_config_parse(NULL);
+	//mono_config_parse(NULL);
 
-	static const char* options[] = {
-		"--soft-breakpoints",
-		"--debugger-agent=transport=dt_socket,address=127.0.0.1:10000"
-        };
+//	static const char* options[] = {
+//		"--soft-breakpoints",
+//		"--debugger-agent=transport=dt_socket,address=127.0.0.1:10000"
+//        };
+//
+//	mono_jit_parse_options(sizeof(options)/sizeof(char*), (char**)options);
 
-	mono_jit_parse_options(sizeof(options)/sizeof(char*), (char**)options);
+//	mono_debug_init(MONO_DEBUG_FORMAT_MONO)
 
-	mono_debug_init(MONO_DEBUG_FORMAT_MONO);
-
-	MonoDomain *domain = mono_jit_init_version("obs-mono", "v4.0.30128");
-//	MonoDomain *domain = mono_jit_init("Mono.Interop");
-	MonoAssembly *interopAssembly =
+//
+	MonoDomain *domain = mono_jit_init_version("obs-mono", "v4.0.30319");
+	MonoAssembly *assembly =
 		mono_domain_assembly_open(domain,
-			"/Users/jrb/Projects/Mono.Interop/Mono.Interop/bin/Debug/Mono.Interop.dll");
+			"/Users/johnbradley/Development/Workspaces/obs/"
+			"obs-studio/plugins/obs-mono/Mono.Interop/bin/"
+			"Debug/Mono.Interop.dll");
 
-	MonoImage *image = mono_assembly_get_image(interopAssembly);
+	MonoImage *image = mono_assembly_get_image(assembly);
 
-	MonoClass *interopClass = mono_class_from_name(image,
+	MonoClass *class = mono_class_from_name(image,
 		"Mono.Interop", "MonoHost");
 
-	MonoObject *interopObject = mono_object_new(domain, interopClass);
+	MonoObject *object = mono_object_new(domain, class);
+	mono_runtime_object_init(object);
 
-	mono_runtime_object_init(interopObject);
+	mono.domain = domain;
 
-	
 	return true;
 }
 
-void
+bool
 obs_module_unload()
 {
+	mono_runtime_cleanup(mono.domain);
+	return false;
 }
